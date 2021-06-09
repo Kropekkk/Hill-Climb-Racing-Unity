125311918c980ca3d15a46e0a81ed5d1ddebaa8a
Kropekkk <Kropekkk@users.noreply.github.com>
2021-06-09 15:33:02 +0200

    FirstScene

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+
+    float movement;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        movement = Input.GetAxis("Horizontal");
+        backTire.AddTorque(-movement * 100 * Time.fixedDeltaTime);
+        frontTire.AddTorque(-movement * 100 * Time.fixedDeltaTime);
+    }
+}
